a6df2e1333ff20fe964fd835003314cd5a043df2
Giovanny Montero <rolencia99@gmail.com>
2026-01-13 22:01:46 -0500

    - Remove unused libraries

--- app/src/main/java/com/castor/bookrecorder/core/presentation/pages/login/LoginScreen.kt
+++ app/src/main/java/com/castor/bookrecorder/core/presentation/pages/login/LoginScreen.kt
@@ -2,7 +2,6 @@ package com.castor.bookrecorder.core.presentation.pages.login
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -37,7 +36,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -189,8 +187,6 @@ fun LoginScreen(
                                     ).show()
                                 }
                             }
-
-
                         },
                         colors = ButtonDefaults.textButtonColors(
                             containerColor = MaterialTheme.colorScheme.primary,
