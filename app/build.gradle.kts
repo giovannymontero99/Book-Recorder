49e9ac576f882a41fb2e9c7f677ebda0e9d95c1a
Giovanny Montero <rolencia99@gmail.com>
2026-01-22 08:47:09 -0500

    Update gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    id("com.google.devtools.ksp") version "2.3.2"
     id("com.google.dagger.hilt.android") version "2.57"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0"
     alias(libs.plugins.google.gms.google.services)
